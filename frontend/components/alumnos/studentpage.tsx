ad6c62089a1d56c7236b7ca9564a056d4f9b904c
elfaldia <diego.17rojas2002@gmail.com>
2024-10-28 16:40:02 -0300

    a

--- frontend/components/alumnos/studentpage.tsx
+++ frontend/components/alumnos/studentpage.tsx
@@ -4,10 +4,7 @@ import { useState, useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
-
-interface StudentPageProps {
-  studentName: string
-}
+import { Moon, Sun } from 'lucide-react'
 
 type GradeCategory = {
   years: number
@@ -21,60 +18,45 @@ const gradeCategories: Record<string, GradeCategory> = {
   C: { years: 1, semestersPerYear: 1, gradesPerSemester: 5 }
 }
 
-const subjectCredits: Record<string, number[][][]> = {
-  A: [
-    [[9, 4, 5, 2, 2, 4], [9, 4, 6, 2, 2, 3]], // 1º y 2º semestres
-    [[11, 9, 4, 3, 3], [11, 6, 6, 3, 4]], // 3º y 4º semestres
-    [[18, 3, 3, 6], [17, 4, 3, 3]], // 5º y 6º semestres
-    [[17, 3, 5, 4], [18, 5, 5, 4]], // 7º y 8º semestres
-    [[16, 5, 5, 4], [16, 2, 5, 3]], // 9º y 10º semestres
-  ],
-  B: [
-    [[17, 32], [26, 32]], // 11º y 12º semestres
-    [[26, 6, 14], [27, 18]], // 13º y 14º semestres
-  ],
-  C: [
-    [[17, 32, 26, 32]]
-  ]
-}
-
-const subjectNames: Record<string, string[][][]> = {
-  A: [
-    [["Anatomía y Embriología I", "Histología I", "Procesos Biológicos I", "Introducción a la Medicina I", "Bioestadística I", "Ciencias Sociales de la Salud"], 
-     ["Anatomía y Embriología II", "Histología II", "Procesos Biológicos II", "Introducción a la Medicina II", "Bioestadística II", "Seguridad Social y Sistemas de salud"]],
-    [["Función Normal y Patológica I", "Neurociencias", "Entrevista Clínica", "Epidemiología", "Investigación Cualitativa"],
-     ["Función Normal y Patológica II", "Microbiología y Parasitología", "Semiología", "Diagnóstico de Salud Participativo", "Investigación Cuantitativa"]],
-    [["Clínica del Adulto I", "Educación para la Salud", "Epidemiología de Enfermedades transmitibles", "Salud Familiar"],
-     ["Clínica del Adulto II", "Ética Médica I", "Medicina Basada en Evidencias", "Salud Ambiental"]],
-    [["Clínica del Adulto III", "Ética Médica II", "Psicopatología I", "Gestión en Salud"], ["Clínica del Adulto IV", "Psicopatología II", "Integración Clínica", "Gestión de Proyectos"]],
-    [["Pediatría", "Psiquiatría I", "Atención Avanzada de Urgencias", "Salud Ocupacional"], ["Derecho y Práctica Médica", "Obstetricia y Ginecología", "Psiquiatría II", "Emergencias y Desastres"]]
-  ],
-  B: [
-    [["Internado de Cirugía", "Internado de Medicina Interna"], ["Internado de Ginecología y Obstetricia", "Internado de Pediatría"]],
-    [["Internado de Especialidad Básica Electiva", "Internado de Especialidad Libre", "Internado de Especialidades Obligatorias"], ["Internado Atención Primaria de Salud", "Internado Rural Interdisciplinario"]]
-  ],
-  C: [
-    [["Internado de Cirugía", "Internado de Medicina Interna", "Internado de Ginecología y Obstetricia", "Internado de Pediatría"]]
-  ]
-}
-
-export function StudentPage({ studentName }: StudentPageProps) {
+export function StudentPage() {
   const [showGrades, setShowGrades] = useState(false)
   const [showStudentButton, setShowStudentButton] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isProfessor, setIsProfessor] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [studentName, setStudentName] = useState('')
+  const [isDarkMode, setIsDarkMode] = useState(false)
   const [grades, setGrades] = useState<Record<string, number[][][]>>({
     A: [
-      [Array(6).fill(7), Array(6).fill(7)],  
-      [Array(5).fill(7), Array(5).fill(7)],  
-      [Array(4).fill(7), Array(4).fill(7)],  
-      [Array(4).fill(7), Array(4).fill(7)],  
-      [Array(4).fill(7), Array(4).fill(7)]   
+      [Array(6).fill(7), Array(6).fill(7)],
+      [Array(5).fill(7), Array(5).fill(7)],
+      [Array(4).fill(7), Array(4).fill(7)],
+      [Array(4).fill(7), Array(4).fill(7)],
+      [Array(4).fill(7), Array(4).fill(7)],
     ],
     B: [
-      [Array(2).fill(7), Array(2).fill(7)],  
-      [Array(3).fill(7), Array(2).fill(7)]  
-    ],
+      [Array(2).fill(7), Array(2).fill(7)],
+      [Array(3).fill(7), Array(2).fill(7)]
+   ],
     C: [[Array(4).fill(0)]]
-  }) 
+  })
+  const [subjectNames, setSubjectNames] = useState<Record<string, string[][][]>>({
+    A: [
+      [["Anatomía y Embriología I", "Histología I", "Procesos BiológicosI", "Introducción a la Medicina I", "Bioestadística I", "Ciencias Sociales de la Salud"], ["Anatomía y Embriología II", "Histología II", "Procesos BiológicosII", "Introducción a la Medicina II", "Bioestadística II", "Seguridad Social y Sistemas de salud"]],
+      [["Función Normal y Patológica I", "Neurociencias", "Entrevista Clínica", "Epidemiología", "Investigación Cualitativa"], ["Función Normal y Patológica II", "Microbiología y Parasitología", "Semiología", "Diagnóstico de Salud Participativo", "Investigación Cuantitativa"]],
+      [["Clínica del Adulto I", "Educación para la Salud", "Epidemiología de Enfermedades transmitibles", "Salud Familiar"],["Clínica del Adulto II", "Ética Médica I", "Medicina Basada en Evidencias", "Salud Ambiental"]],
+      [["Clínica del Adulto III", "Ética Médica II", "Psicopatología I", "Gestión en Salud"],["Clínica del Adulto IV", "Psicopatología II", "Integración Clínica", "Gestión de Proyectos"]],
+      [["Pediatría", "Psiquiatría I", "Atención Avanzada de Urgencias", "SaludOcupacional"],["Derecho y Práctica Médica", "Obstetricia y Ginecología", "Psiquiatría II", "Emergencias y Desastres"]],
+    ],
+    B: [
+      [["Internado de Cirugía","Internado de Medicina Interna"],["Internado de Ginecología y Obstetricia","Internado de Pediatría"]],
+      [["Internado de Especialidad Básica Electiva","Internado de Especialidad Libre","Internado de Especialidades Obligatorias"],["Internado Atención Primaria de Salud","Internado Rural Interdisciplinario"]],
+    ],
+    C: [
+      [["Internado de Cirugía","Internado de Medicina Interna","Internado de Ginecología y Obstetricia","Internado de Pediatría"]],
+    ],
+  })   
   const [finalGrade, setFinalGrade] = useState(0)
   const [expandedCategories, setExpandedCategories] = useState<Record<string, boolean>>({ A: false, B: false, C: false })
 
@@ -85,6 +67,25 @@ export function StudentPage({ studentName }: StudentPageProps) {
       newGrades[category][year][semester][index] = newValue
       return newGrades
     })
+  }
+
+  const semesterCredits = [26, 27, 30, 30, 30, 27, 29, 32, 30, 26, 49, 58, 47, 45]
+
+  const subjectCredits: Record<string, number[][][]> = {
+    A: [
+      [[9, 4, 5, 2, 2, 4], [9, 4, 6, 2, 2, 3]],
+      [[11, 9, 4, 3, 3], [11, 6, 6, 3, 4]],
+      [[18, 3, 3, 6], [17, 4, 3, 3]],
+      [[17, 3, 5, 4], [18, 5, 5, 4]],
+      [[16, 5, 5, 4], [16, 2, 5, 3]],
+    ],
+    B: [
+      [[17, 32], [26, 32]],
+      [[26, 6, 14], [27, 18]],
+    ],
+    C: [
+        [[17, 32, 26, 32]]
+    ]
   }
 
   useEffect(() => {
@@ -122,19 +123,33 @@ export function StudentPage({ studentName }: StudentPageProps) {
     setExpandedCategories(prev => ({ ...prev, [category]: !prev[category] }))
   }
 
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (username === 'Gabriel Vergara' && password === '1234') {
+      setIsLoggedIn(true)
+      setIsProfessor(false)
+      setStudentName(username)
+    } else if (username === 'profesor' && password === '4321') {
+      setIsLoggedIn(true)
+      setIsProfessor(true)
+    } else {
+      alert('Credenciales incorrectas')
+    }
+  }
+
   const renderGradeInputs = (category: string, year: number, semester: number) => (
     <div className="grid grid-cols-6 gap-4 mb-4">
       {grades[category] && grades[category][year] && grades[category][year][semester] ? (
         grades[category][year][semester].map((grade, index) => (
           <div key={index} className="flex flex-col items-center">
-            <span className="text-xs text-gray-600 mb-1">
+            <span className={`text-xs mb-1 ${isDarkMode ? 'text-gray-300' : 'text-gray-600'}`}>
               {subjectNames[category][year][semester][index] || `Asignatura ${index + 1}`}
             </span>
             <Input
               type="number"
               value={grade || ''}
               onChange={(e) => updateGrade(category, year, semester, index, e.target.value)}
-              className="w-16 text-black border border-gray-500 rounded"
+              className={`w-16 border rounded ${isDarkMode ? 'bg-gray-700 text-white border-gray-600' : 'bg-white text-black border-gray-300'}`}
               min={1}
               max={7}
               step={0.1}
@@ -142,32 +157,34 @@ export function StudentPage({ studentName }: StudentPageProps) {
           </div>
         ))
       ) : (
-        <p>No hay datos disponibles para este semestre.</p>
+        <p className={isDarkMode ? 'text-gray-300' : 'text-gray-600'}>No hay datos disponibles para este semestre.</p>
       )}
     </div>
-  )
+  )   
 
   const renderCategory = (category: string) => (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value={category}>
         <AccordionTrigger
-          className="text-lg font-semibold text-black py-2"
+          className={`text-lg font-semibold py-2 ${isDarkMode ? 'text-white' : 'text-black'}`}
           onClick={() => toggleCategory(category)}
         >
-          Notas {category} ({category === 'A' ? '60%' : category === 'B' ? '30%' : '10%'})
+          {category === 'A' ? 'Notas de primero a quinto año (60%)' :
+           category === 'B' ? 'Notas de sexto a septimo año [internados] (30%)' :
+           'Notas de pregrado de espezializacion [4/9] (10%)'}
         </AccordionTrigger>
         {expandedCategories[category] && (
           <AccordionContent>
             {Array.from({ length: gradeCategories[category].years }).map((_, yearIndex) => (
               <Accordion key={yearIndex} type="single" collapsible className="mb-4">
                 <AccordionItem value={`year-${yearIndex}`}>
-                  <AccordionTrigger className="text-md font-medium text-black">
+                  <AccordionTrigger className={`text-md font-medium ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>
                     {category === 'C' ? 'Notas' : `${yearIndex + (category === 'B' ? 6 : 1)}º Año`}
                   </AccordionTrigger>
                   <AccordionContent>
                     {Array.from({ length: gradeCategories[category].semestersPerYear }).map((_, semesterIndex) => (
                       <div key={semesterIndex} className="mb-4">
-                        <h4 className="text-sm font-medium mb-2 text-black">
+                        <h4 className={`text-sm font-medium mb-2 ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>
                           {category === 'C' ? '' : `Semestre ${semesterIndex + 1}`}
                         </h4>
                         {renderGradeInputs(category, yearIndex, semesterIndex)}
@@ -183,18 +200,61 @@ export function StudentPage({ studentName }: StudentPageProps) {
     </Accordion>
   )
 
-  return (
-    <div className="flex h-screen bg-gray-200">
-      <div className="w-64 bg-blue-600 p-4 flex flex-col">
+  const renderLogin = () => (
+    <div
+      className={`flex justify-center items-center h-screen bg-cover bg-center ${isDarkMode ? 'bg-gray-900' : ''}`}
+      style={{ backgroundImage: isDarkMode ? 'none' : 'url(https://postgrado.ucn.cl/wp-content/uploads/2023/06/Escudo-UCN-EIMG_7894-scaled.jpg)' }}
+    >
+      <form onSubmit={handleLogin} className={`p-6 rounded shadow-md w-80 ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
+        <h2 className={`text-2xl font-bold mb-4 text-center ${isDarkMode ? 'text-white' : 'text-black'}`}>Login</h2>
+        <div className="mb-4">
+          <label className={`block text-sm font-bold mb-2 ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`} htmlFor="username">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className={`w-full p-2 border rounded ${isDarkMode ? 'bg-gray-700 text-white border-gray-600' : 'bg-white text-black border-gray-300'}`}
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="mb-6">
+          <label className={`block text-sm font-bold mb-2 ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`} htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={`w-full p-2 border rounded ${isDarkMode ? 'bg-gray-700 text-white border-gray-600' : 'bg-white text-black border-gray-300'}`}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button
+          type="submit"
+          className={`w-full text-white p-2 rounded ${isDarkMode ? 'bg-blue-600 hover:bg-blue-700' : 'bg-blue-500 hover:bg-blue-600'}`}
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  )
+
+  const renderGradesInterface = () => (
+    <div className={`flex h-screen ${isDarkMode ? 'bg-gray-900' : 'bg-gray-200'}`}>
+      <div className={`w-64 p-4 flex  flex-col ${isDarkMode ? 'bg-gray-800' : 'bg-blue-600'}`}>
         <h2 className="text-white text-xl font-bold mb-4">{studentName}</h2>
         <Button
           onClick={() => setShowStudentButton(!showStudentButton)}
-          className="mb-2 bg-black text-white"
+          className={`mb-2 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-black text-white'}`}
         >
           Estudiante
         </Button>
         {showStudentButton && (
-          <Button onClick={() => setShowGrades(!showGrades)} className="bg-black text-white">
+          <Button onClick={() => setShowGrades(!showGrades)} className={isDarkMode ? 'bg-gray-700 text-white' : 'bg-black text-white'}>
             Simular Notas
           </Button>
         )}
@@ -202,22 +262,46 @@ export function StudentPage({ studentName }: StudentPageProps) {
 
       <div className="flex-1 p-8 overflow-y-auto">
         {!showGrades ? (
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h1 className="text-3xl font-bold mb-4 text-black">Bienvenido</h1>
-            <p className="text-gray-800">Este es un mensaje de bienvenida para el estudiante.</p>
+          <div className={`p-6 rounded-lg shadow-md ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
+            <h1 className={`text-3xl font-bold mb-4 ${isDarkMode ? 'text-white' : 'text-black'}`}>Bienvenido</h1>
+            <p className={isDarkMode ? 'text-gray-300' : 'text-gray-800'}>Este es un mensaje de bienvenida para el estudiante.</p>
           </div>
         ) : (
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h1 className="text-3xl font-bold mb-6 text-black">Simulación de Notas</h1>
+          <div className={`p-6 rounded-lg shadow-md ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
+            <h1 className={`text-3xl font-bold mb-6 ${isDarkMode ? 'text-white' : 'text-black'}`}>Simulación de Notas</h1>
             {renderCategory('A')}
             {renderCategory('B')}
             {renderCategory('C')}
-            <div className="mt-6 p-4 bg-gray-200 rounded-lg">
-              <p className="font-bold text-xl text-center text-black">Nota Final: {finalGrade}</p>
+            <div className={`mt-6 p-4 rounded-lg ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
+              <p className={`font-bold text-xl text-center ${isDarkMode ? 'text-white' : 'text-black'}`}>Nota Final: {finalGrade}</p>
             </div>
           </div>
         )}
       </div>
+    </div>
+  )
+
+  const renderProfessorInterface = () => (
+    <div className={`flex h-screen items-center justify-center ${isDarkMode ? 'bg-gray-900' : 'bg-gray-200'}`}>
+      <h1 className={`text-3xl font-bold ${isDarkMode ? 'text-white' : 'text-black'}`}>Bienvenido Profesor</h1>
+    </div>
+  )
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
+  return (
+    <div className={isDarkMode ? 'dark' : ''}>
+      <button
+        onClick={toggleDarkMode}
+        className={`fixed top-4 right-4 p-2 rounded-full ${
+          isDarkMode ? 'bg-yellow-400 text-gray-900' : 'bg-gray-800 text-yellow-400'
+        }`}
+      >
+        {isDarkMode ? <Sun size={24} /> : <Moon size={24} />}
+      </button>
+      {isLoggedIn ? (isProfessor ? renderProfessorInterface() : renderGradesInterface()) : renderLogin()}
     </div>
   )
 }
