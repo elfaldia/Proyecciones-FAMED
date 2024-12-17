2fd13d6392292812c2fbbb0318f49ffabc4e26f7
frutillitaaaa <daniela.castro02@alumnos.ucn.cl>
2024-12-17 00:10:13 -0300

    sidebar

--- frontend/app/profesor/layout.tsx
+++ frontend/app/profesor/layout.tsx
@@ -2,6 +2,8 @@
 import Link from 'next/link'
 import './styles.css'
 import useSessionStore from '@/stores/useSessionStore'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { ProfSidebar } from  '@/components/prof-sidebar'
 
 export default function RootLayout({
   children,
@@ -15,26 +17,12 @@ export default function RootLayout({
   }
 
   return (
-    <html lang="es">
-      <body>
-        <div className="app-container">
-            <div className="sidebar">
-              <h1 className="sidebar-title">Nombre del Profesor</h1>
-              <Link href="../profesor/estudiantes" passHref>      
-                <button className="sidebar-button">Estudiantes</button>
-              </Link>
-            
-              <Link  href="/" passHref legacyBehavior>
-                <button onClick={onHandleCerrarSesion} className="sidebar-bottom-button">Cerrar Sesión</button>
-              </Link>
-            
-            </div>
-            
-            <div className="main-content">
-            {children}
-            </div>
-        </div>
-      </body>
-    </html>
+    <SidebarProvider>
+      <ProfSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   )
 }
