86ca191f5f5ae1d0418081afed038dfb619b1c3d
SebaVCH <109924179+SebaVCH@users.noreply.github.com>
2024-12-18 11:30:56 -0300

    Cambios de logica y mensaje de bienvenida
    se cambio el mensaje de bienvenida en el menu de estudiante y del profesor. Ademas se cambio el nombre del formato "creditos" a "creditosAsociados" para la clase "ramo". Y por ultimo se valido el cierre de sesion en la vista de profesor

--- backend/internal/response/ramo_response.go
+++ backend/internal/response/ramo_response.go
@@ -5,6 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type RamoResponse struct {
 	Id       primitive.ObjectID `bson:"_id" json:"_id"`
 	Nombre   string             `json:"nombre" bson:"nombre"`
-	Creditos int                `json:"creditos" bson:"creditos"`
+	Creditos int                `json:"creditosAsociados" bson:"creditosAsociados"`
 	Semestre string             `json:"semestre" bson:"semestre"`
 }
